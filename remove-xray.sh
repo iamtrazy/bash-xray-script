ec35ba4f5904e43f5d290af4a90cac2dc9339163
iamtrazy <73080739+iamtrazy@users.noreply.github.com>
2021-05-03 13:55:09 +0530

    Create remove-xray.sh

---
+++ remove-xray.sh
@@ -0,0 +1,9 @@
+#Remove old certificates
+rm -rf /etc/xray
+#remove acme.sh
+acme.sh --uninstall
+rm -rf ~/.acme.sh
+#remove json configs
+rm -rf  /usr/local/etc/xray
+#remove xray-core
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ remove
