01ec8a6637e086a81a30a037f452da47a024dd46
iamtrazy <iamtrazy@protonmail.com>
2021-07-23 08:47:45 +0530

    Update xray-whatever.sh

--- xray-whatever.sh
+++ xray-whatever.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
 
-#input uuid & domain
-
 echo Enter a valid gen4 UUID:
 read UUID
-
-#configure timezone to sri lanka standards
 
 rm -rf /etc/localtime
 cp /usr/share/zoneinfo/Asia/Colombo /etc/localtime
 date -R
 
-#disable ubuntu firewall
 ufw disable
-
-#running xray install script for linux - systemd
 
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
 
-#adding new configuration files 
-
 rm -rf /usr/local/etc/xray/config.json
-cat << EOF > /usr/local/etc/xray/config.json
+cat << EOF > /usr/local/etc/xray/CONF0.json
 {
   "log": {
     "loglevel": "warning"
-  },
+  }
+}
+EOF
+cat << EOF > /usr/local/etc/xray/CONF1.json
+{
   "inbounds": [
     {
       "port": 443,
       "protocol": "vless",
+      "tag": "XTLS",
       "settings": {
         "clients": [
           {
             "id": "$UUID",
             "flow": "xtls-rprx-direct",
-            "level": 0,
-            "email": "love@example.com"
+            "level": 0
           }
         ],
         "decryption": "none",
@@ -82,12 +77,12 @@ cat << EOF > /usr/local/etc/xray/config.json
       "port": 1310,
       "listen": "127.0.0.1",
       "protocol": "trojan",
+      "tag": "TROJAN",
       "settings": {
         "clients": [
           {
             "password": "$UUID",
-            "level": 0,
-            "email": "love@example.com"
+            "level": 0
           }
         ],
         "fallbacks": [
@@ -108,12 +103,12 @@ cat << EOF > /usr/local/etc/xray/config.json
       "port": 1234,
       "listen": "127.0.0.1",
       "protocol": "vless",
+      "tag": "VLESS_WEBSOCKET",
       "settings": {
         "clients": [
           {
             "id": "$UUID",
-            "level": 0,
-            "email": "love@example.com"
+            "level": 0
           }
         ],
         "decryption": "none"
@@ -131,12 +126,12 @@ cat << EOF > /usr/local/etc/xray/config.json
       "port": 2345,
       "listen": "127.0.0.1",
       "protocol": "vmess",
+      "tag": "VMESS_TCP",
       "settings": {
         "clients": [
           {
             "id": "$UUID",
-            "level": 0,
-            "email": "love@example.com"
+            "level": 0
           }
         ]
       },
@@ -160,12 +155,12 @@ cat << EOF > /usr/local/etc/xray/config.json
       "port": 3456,
       "listen": "127.0.0.1",
       "protocol": "vmess",
+      "tag": "VMESS_WEBSOCKET",
       "settings": {
         "clients": [
           {
             "id": "$UUID",
-            "level": 0,
-            "email": "love@example.com"
+            "level": 0
           }
         ]
       },
@@ -177,17 +172,21 @@ cat << EOF > /usr/local/etc/xray/config.json
           "path": "/vmessws"
         }
       }
-    },
+    }
+  ]
+}
+EOF
+cat << EOF > /usr/local/etc/xray/CONF2.json
+{
+  "inbounds": [
     {
-      "listen": "0.0.0.0",
       "port": 80,
       "protocol": "vmess",
+      "tag": "VMESS_HTTP",
       "settings": {
         "clients": [
           {
-            "id": "$UUID",
-            "level": 0,
-            "email": "love@example.com"
+            "id": "$UUID"
           }
         ]
       },
@@ -196,23 +195,20 @@ cat << EOF > /usr/local/etc/xray/config.json
         "tcpSettings": {
           "header": {
             "type": "http",
-            "request": {
+            "response": {
               "version": "1.1",
-              "method": "GET",
-              "path": [
-                "/"
-              ],
+              "status": "200",
+              "reason": "OK",
               "headers": {
-                "Host": [
-                  "c.whatsapp.net",
-                  "m.facebook.com"
+                "Content-Type": [
+                  "application/octet-stream",
+                  "video/mpeg",
+                  "application/x-msdownload",
+                  "text/html",
+                  "application/x-shockwave-flash"
                 ],
-                "User-Agent": [
-                  "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-                  "Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_2 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/53.0.2785.109 Mobile/14A456 Safari/601.1.46"
-                ],
-                "Accept-Encoding": [
-                  "gzip, deflate"
+                "Transfer-Encoding": [
+                  "chunked"
                 ],
                 "Connection": [
                   "keep-alive"
@@ -221,10 +217,15 @@ cat << EOF > /usr/local/etc/xray/config.json
               }
             }
           }
-        }
+        },
+        "security": "none"
       }
     }
-  ],
+  ]
+}
+EOF
+cat << EOF > /usr/local/etc/xray/CONF3.json
+{
   "outbounds": [
     {
       "protocol": "freedom"
@@ -232,9 +233,6 @@ cat << EOF > /usr/local/etc/xray/config.json
   ]
 }
 EOF
-
-
-#accuring a ssl certificate (self-sigend openssl)
 
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
     -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" \
@@ -244,12 +242,11 @@ cp xray.key /etc/xray/xray.key
 cp xray.crt /etc/xray/xray.crt
 chmod 644 /etc/xray/xray.key
 
-#enable xray startup
-
+sed -i 's/-config/-confdir/g' /etc/systemd/system/xray.service.d/10-donot_touch_single_conf.conf
+sed -i 's/\/config.json/\//g' /etc/systemd/system/xray.service.d/10-donot_touch_single_conf.conf
+systemctl daemon-reload
 systemctl enable xray
 systemctl restart xray
-
-#install bbr
 
 mkdir ~/across
 git clone https://github.com/teddysun/across ~/across
